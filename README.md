24f2cb62bb68af65ca10102e9b8d99b1f99684a3
tektohub <boudouros@gmail.com>
2021-11-29 17:23:42 +0100

    init project tree

---
+++ README.md
@@ -0,0 +1,7 @@
+# whitelist app with react box
+
+Projet made with truffle unbox react
+
+Whitelist for Voting system, little changes on solc version, and event added, no more run at init
+
+Done to test deployment on public server
--- truffle-config.js
+++ truffle-config.js
@@ -18,63 +18,45 @@
 *
 */
  
+const path = require("path");
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
  
 module.exports = {
- /**
-  * Networks define how you connect to your ethereum client and let you set the
-  * defaults web3 uses to send transactions. If you don't specify one truffle
-  * will spin up a development blockchain for you on port 9545 when you
-  * run `develop` or `test`. You can ask a truffle command to use a specific
-  * network from the command line, e.g
-  *
-  * $ truffle test --network <network-name>
-  */
- 
+ contracts_build_directory: path.join(__dirname, "client/src/contracts"),
  networks: {
-   // Useful for testing. The `development` name is special - truffle uses it by default
-   // if it's defined here and no other network is specified at the command line.
-   // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-   // tab if you use this network and you must also set the `host`, `port` and `network_id`
-   // options below to some value.
-   //
+
    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "5777",       // Any network (default: none)
-   },
-   rinkeby: {
-     provider: function() {
-       return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
-     },
-     network_id: 4
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*", 
    },
    ropsten: {
      provider: function() {
        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
      },
      network_id: 3
-   }
+   },
  },
  
  // Set default mocha options here, use special reporters etc.
- mocha: {
-   // timeout: 100000
- },
- 
+  mocha: {
+    // timeout: 100000
+  },
+
  // Configure your compilers
- compilers: {
-   solc: {
-     version: "0.6.11",    // Fetch exact version from solc-bin (default: truffle's version)
-     // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-     settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-       enabled: false,
-       runs: 200
-       },
-     evmVersion: "byzantium"
-     }
-   },
- }
-};
+  compilers: {
+    solc: {
+      version: "0.8.7",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      //  evmVersion: "byzantium"
+       }
+    }
+  },
+
+  };
